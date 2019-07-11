e709fb5aa3b94e6aa2a4108c52acea7cce611fdc
MiglyA <zq6295migly@gmail.com>
2019-07-11 15:09:06 +0900

    [update]thread追加

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import threading
+import readchar
 import pygame
 import sys
+import serial
 
 
 class SightQuiz:
     def __init__(self):
         self.width = 1280
         self.height = 640
+        self.input_data = 0
         
         pygame.init()
-        pygame.display.set_caption('Sight Quiz!')
+        pygame.display.set_caption("Sight Quiz!")
         self.window = pygame.display.set_mode((self.width, self.height))
         self.window.fill((255, 255, 255))
         self.sight_img = pygame.transform.scale(pygame.image.load("./img/sight.png").convert_alpha(), (50, 50))
         pygame.display.update()
+        
+        try:
+            self.serial_instance = serial.Serial("/dev/ttyS0", 9600, timeout=0.1)
+            print "connected"
+        except IOError:
+            pass
+    
+    def wait_key(self):
+        while True:
+            __input_data__ = readchar.readchar()
+            if __input_data__ == 'q':
+                sys.exit(1)
+            self.input_data = __input_data__
     
     @staticmethod
     def is_kill():
@@ -44,14 +61,20 @@ class SightQuiz:
         self.window.blit(self.sight_img, (self.width * 1 / 6 - 25, self.height / 2 - 25))
         self.window.blit(self.sight_img, (self.width * 5 / 6 - 25, self.height / 2 - 25))
     
+    def sight_target(self):
+        pygame.draw.circle(self.window, (0, 255, 0), (self.width * int(self.input_data) / 9, self.height / 2), 30)
+    
     def update(self):
         while True:
             self.is_kill()
             self.window.fill((255, 255, 255))
             self.base()
+            self.sight_target()
             pygame.display.update()
 
 
 if __name__ == '__main__':
     sight_quiz = SightQuiz()
+    thread = threading.Thread(target=sight_quiz.wait_key)
+    thread.start()
     sight_quiz.update()
